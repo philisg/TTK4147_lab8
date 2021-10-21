7684a3633158d1d434c2871244f5cc8c6313a6fd
simennett <simennett@gmail.com>
2021-10-21 16:51:50 +0200

    ha

--- oppgd/main.c
+++ oppgd/main.c
@@ -13,6 +13,7 @@
 
 RT_MUTEX mutexA, mutexB;
 RT_TASK task_l , task_m, task_h;
+RT_SEM sem;
 
 void busy_wait_us(unsigned long delay){
     for(; delay > 0; delay--){
@@ -21,13 +22,19 @@ void busy_wait_us(unsigned long delay){
 }
 
 void low_function(void){
+    rt_sem_p(&sem, TM_INFINITE);
+
     rt_mutex_acquire(&mutexA, TM_INFINITE);
+
     rt_printf("LOW THREAD HAS STARTED! \n");
     busy_wait_us(3*TIME_UNIT);
+
     rt_mutex_acquire(&mutexB, TM_INFINITE);
     busy_wait_us(3*TIME_UNIT);
+
     rt_mutex_release(&mutexB);
     rt_mutex_release(&mutexA);
+
     busy_wait_us(1*TIME_UNIT);
     rt_printf("LOW THREAD HAS FINISHED! \n");
 }
@@ -40,6 +47,8 @@ void medium_function(void){
 }
 
 void high_function(void){
+    rt_sem_p(&sem, TM_INFINITE);
+
     rt_task_sleep(TIME_UNIT * 1);
     rt_mutex_acquire(&mutexB, TM_INFINITE);
     rt_printf("HIGH THREAD HAS STARTED! \n");
@@ -58,22 +67,24 @@ int main(){
 	rt_print_auto_init(1);
 	rt_mutex_create(&mutexA, NULL);
     rt_mutex_create(&mutexB, NULL);
+    rt_sem_create(&sem,"Semaphore", 1, S_FIFO);
 
-	rt_task_create(&task_l, NULL, 0, LOW, T_CPU(0));
+	rt_task_create(&task_l, "task_low", 0, 40, T_CPU(0));
 	//rt_task_create(&task_m, NULL, 0, MEDIUM, T_CPU(0));
-	rt_task_create(&task_h, NULL, 0, HIGH, T_CPU(0));
+	rt_task_create(&task_h, "task_high", 0, 50, T_CPU(0));
 
 	rt_task_start(&task_l, (void*)low_function, NULL);
 	//rt_task_start(&task_m, (void*)medium_function, NULL);
 	rt_task_start(&task_h, (void*)high_function, NULL);
 
 	rt_task_sleep(100000000);
-	//rt_sem_broadcast(&sem1);
+	rt_sem_broadcast(&sem);
 	rt_task_sleep(100000000);
 
     while(1);
 
 	rt_mutex_delete(&mutexA);
     rt_mutex_delete(&mutexB);
+     rt_sem_delete(&sem);
 	return 0;
 }
