ca3d9155d35442b23e1bf75ccfdb9a51a65d478d
Philip Gausaker <philisg@stud.ntnu.no>
2021-10-21 14:29:15 +0200

    trash

--- main.c
+++ main.c
@@ -19,18 +19,18 @@ int set_cpu(int cpu_number);
 void responseTask1(void) {
 	set_cpu(T_CPU(0));
     rt_sem_p(&sem1, TM_INFINITE);
-    rt_sem_v(&sem1);
     rt_timer_spin(100*1000*1000); //100ms
+    rt_sem_v(&sem1);
     rt_printf("Task1\n");
-    return 0;
 }
 
 void responseTask2(void) {
 	set_cpu(T_CPU(0));
     rt_sem_p(&sem1, TM_INFINITE);
     rt_timer_spin(100*1000*1000); //100ms
+    rt_sem_v(&sem1);
     rt_printf("Task2\n");
-    return 0;
+
 }
 
 
