824f71d02c3332c6a57a3f7fb54f82cdc70ea154
simennett <simennett@gmail.com>
2021-10-21 16:28:47 +0200

    ha

--- oppgc/main.c
+++ oppgc/main.c
@@ -12,41 +12,36 @@
 #define TIME_UNIT 100
 
 RT_MUTEX mutex;
+RT_TASK task_l , task_m, task_h;
 
 void busy_wait_us(unsigned long delay){
-		for(; delay > 0; delay--){
-			rt_timer_spin(1000);
-		}
+    for(; delay > 0; delay--){
+        rt_timer_spin(1000);
+    }
 }
 
-void LOW(void){
-	//while(1){
-		rt_mutex_acquire(&mutex, TM_INFINITE);
-		rt_printf("LOW THREAD HAS STARTED! \n");
-		busy_wait_us(3*TIME_UNIT);
-		rt_mutex_release(&mutex);
-		rt_printf("LOW THREAD HAS FINISHED! \n");
-	//}
+void low_function(void){
+    rt_mutex_acquire(&mutex, TM_INFINITE);
+    rt_printf("LOW THREAD HAS STARTED! \n");
+    busy_wait_us(3*TIME_UNIT);
+    rt_mutex_release(&mutex);
+    rt_printf("LOW THREAD HAS FINISHED! \n");
 }
 
 void MEDIUM(void){
-	//while(1){
-		rt_task_sleep(TIME_UNIT * 1000);
-		rt_printf("MEDIUM THREAD HAS STARTED! \n");
-		busy_wait_us(5*TIME_UNIT);
-		rt_printf("MEDIUM THREAD HAS FINISHED! \n");
-	//}
+    rt_task_sleep(TIME_UNIT * 1000);
+    rt_printf("MEDIUM THREAD HAS STARTED! \n");
+    busy_wait_us(5*TIME_UNIT);
+    rt_printf("MEDIUM THREAD HAS FINISHED! \n");
 }
 
 void HIGH(void){
-	//while(1){
-		rt_task_sleep(TIME_UNIT * 2000);
-		rt_mutex_acquire(&mutex, TM_INFINITE);
-		rt_printf("HIGH THREAD HAS STARTED! \n");
-		busy_wait_us(2*TIME_UNIT);
-		rt_mutex_release(&mutex);
-		rt_printf("HIGH THREAD HAS FINISHED! \n");
-	//}
+    rt_task_sleep(TIME_UNIT * 2000);
+    rt_mutex_acquire(&mutex, TM_INFINITE);
+    rt_printf("HIGH THREAD HAS STARTED! \n");
+    busy_wait_us(2*TIME_UNIT);
+    rt_mutex_release(&mutex);
+    rt_printf("HIGH THREAD HAS FINISHED! \n");
 }
 
 int main(){
@@ -55,11 +50,11 @@ int main(){
 	rt_print_auto_init(1);
 	rt_mutex_create(&mutex, NULL);
 
-	RT_TASK task_l , task_m, task_h;
 
-	rt_task_create(&task_l, NULL, 0, LOW, T_CPU(0));
-	rt_task_create(&task_m, NULL, 0, MEDIUM, T_CPU(0));
-	rt_task_create(&task_h, NULL, 0, HIGH, T_CPU(0));
+
+	rt_task_create(&task_l, NULL, 0, low-task, T_CPU(0));
+	rt_task_create(&task_m, NULL, 0, med-task, T_CPU(0));
+	rt_task_create(&task_h, NULL, 0, high-task, T_CPU(0));
 
 	rt_task_start(&task_l, (void*)LOW, NULL);
 	rt_task_start(&task_m, (void*)MEDIUM, NULL);
