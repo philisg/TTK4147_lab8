184076af750adbfc74b0904fe8fc2649ed3838c5
Philip Gausaker <philisg@stud.ntnu.no>
2021-10-21 14:50:35 +0200

    trash

--- main.c
+++ main.c
@@ -10,22 +10,39 @@
 #include <sys/mman.h>
 #include <native/sem.h>
 
-RT_TASK taskresponse_thread_1;
-RT_TASK taskresponse_thread_2;
-RT_SEM sem1, sem2;
+RT_TASK HIGH_thread;
+RT_TASK MEDIUM_thread;
+RT_TASK LOW_thread;
+RT_SEM sem1;
 int set_cpu(int cpu_number);
 
-
-void responseTask1(void) {
+void busy_wait_us(unsigned long delay){
+    for(; delay > 0; delay--){
+        rt_timer_spin(1000);
+    }
+}
+void HIGH(void) {
 	set_cpu(T_CPU(0));
+    rt_task_sleep(2000);
     rt_sem_p(&sem1, TM_INFINITE);
-    rt_printf("Task1\n");
+    busy_wait_us(2);
+    rt_sem_v(&sem1);
 }
 
-void responseTask2(void) {
+void MEDIUM(void) {
+	set_cpu(T_CPU(0));
+    rt_task_sleep(1000);
+    busy_wait_us(5);
+    rt_printf("Tas MEDIUM!\n");
+
+}
+
+void LOW(void) {
 	set_cpu(T_CPU(0));
     rt_sem_p(&sem1, TM_INFINITE);
-    rt_printf("Task2\n");
+    rt_printf("LOW\n");
+    busy_wait_us(3);
+    rt_sem_v(&sem1);
 
 }
 
@@ -40,11 +57,13 @@ int main (void){
     //rt_sem_create(&sem2,"Semaphore2", 0, S_FIFO);
 
 
-    rt_task_create(&taskresponse_thread_1, "task1", 0, 55, T_CPU(0));//creating task, 50=priority
-    rt_task_create(&taskresponse_thread_2, "task2", 0, 50, T_CPU(0));//creating task
+    rt_task_create(&HIGH_thread, "task1", 0, 55, T_CPU(0));//creating task, 50=priority
+    rt_task_create(&MEDIUM_thread, "task2", 0, 50, T_CPU(0));//creating task
+    rt_task_create(&LOW_thread, "task3", 0, 40, T_CPU(0));//creating task  rt_task_create(&MEDIUM, "task2", 0, 50, T_CPU(0));//creating task
         
-    rt_task_start(&taskresponse_thread_1, &responseTask1, NULL); //start the task
-    rt_task_start(&taskresponse_thread_2, &responseTask2, NULL); //start the task
+    rt_task_start(&HIGH, &HIGH_thread, NULL); //start the task_
+    rt_task_start(&MEDIUM, &MEDIUM_thread, NULL); //start the task
+    rt_task_start(&LOW, &LOW_thread, NULL); //start the task
 
     rt_task_sleep(500000000); //500ms
 
