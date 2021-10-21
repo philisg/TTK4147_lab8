b0291390d6da42a32da5bc2e79fe119d7bba4a86
Philip Gausaker <philisg@stud.ntnu.no>
2021-10-21 14:08:18 +0200

    Send it

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ void responseTask1(void) {
 	set_cpu(T_CPU(0));
     rt_sem_p(&sem1, TM_INFINITE);
     rt_timer_spin(100*1000*1000); //100ms
+    rt_printf("Task1");
     return 0;
 }
 
@@ -28,6 +29,8 @@ void responseTask2(void) {
 	set_cpu(T_CPU(0));
     rt_sem_p(&sem1, TM_INFINITE);
     rt_timer_spin(100*1000*1000); //100ms
+    rt_printf("Task2");
+
     return 0;
 }
 
