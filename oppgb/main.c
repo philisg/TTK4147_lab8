bd37670f73c6934443f0d19ca1d6c53052e41fe1
simennett <simennett@gmail.com>
2021-10-22 09:24:12 +0200

    ha

--- oppgb/main.c
+++ oppgb/main.c
@@ -14,6 +14,8 @@
 #define TIME_UNIT 10000
 
 RT_SEM sem1;
+RT_TASK task_l , task_m, task_h;
+    
 
 void busy_wait_us(unsigned long delay){
 		for(; delay > 0; delay--){
@@ -47,17 +49,19 @@ void high_function(void){
 
 int main(){
 	mlockall(MCL_CURRENT | MCL_FUTURE);
+    rt_print_auto_init(1);
 	rt_task_shadow(NULL, NULL, 90, T_CPU(0));
-	rt_print_auto_init(1);
+
 	rt_sem_create(&sem1, NULL, 1, NULL);
-	RT_TASK task_l , task_m, task_h;
-	rt_task_create(&task_l, NULL, 0, LOW, T_CPU(0));
-	rt_task_create(&task_m, NULL, 0, MEDIUM, T_CPU(0));
-	rt_task_create(&task_h, NULL, 0, HIGH, T_CPU(0));
+
+	rt_task_create(&task_l, NULL, 0, 60, T_CPU(0));
+	rt_task_create(&task_m, NULL, 0, 70, T_CPU(0));
+	rt_task_create(&task_h, NULL, 0, 80, T_CPU(0));
+
 	rt_task_start(&task_l, (void*)low_function, NULL);
 	rt_task_start(&task_m, (void*)medium_function, NULL);
 	rt_task_start(&task_h, (void*)high_function, NULL);
-    
+
 	rt_task_sleep(100000000);
 	//rt_sem_broadcast(&sem1);
 	rt_task_sleep(100000000);
